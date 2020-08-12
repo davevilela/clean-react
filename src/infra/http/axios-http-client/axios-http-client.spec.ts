dffa2da1ac7fd351fadd94d3e04e35e937b50c0f
Davi Vilela <davivilelaa@gmail.com>
2020-08-11 22:10:09 -0300

    test: ensure AxiosHttpPostClient calls axios with correct url and verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -10,10 +10,10 @@ const makeSut = (): AxiosHttpClient => {
 }
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct url', async () => {
+  test('Should call axios with correct url and verb', async () => {
     const sut = makeSut()
     const url = faker.internet.url()
     await sut.post({ url })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })
